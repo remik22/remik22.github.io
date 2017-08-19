aaeb9e09be45e71b9a260f6172a88422e2cf10c7
Remigiusz Lagoda <remik22@gmail.com>
2017-08-19 10:39:03 +0200

    data

---
+++ cssrozszerzony/warsztaty/JS/zmienne/js/datatypes.js
@@ -0,0 +1,30 @@
+'use strict'
+
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata = "Zarabiam: " + (wyplata + premia);
+
+console.log(calkowitaWyplata);
+
+
+//Typ - łańcuch znaków
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+/*typ logiczny - boolean*/
+
+var smog = true;
+console.log(smog);
+
+/* typ specjalny - undefined / null */
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
