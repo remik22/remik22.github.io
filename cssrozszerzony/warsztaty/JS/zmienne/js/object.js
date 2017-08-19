1b80da90699e589b7e193f2c8963bbce26a7671d
Remigiusz Lagoda <remik22@gmail.com>
2017-08-19 14:34:01 +0200

    obj

--- cssrozszerzony/warsztaty/JS/zmienne/js/object.js
+++ cssrozszerzony/warsztaty/JS/zmienne/js/object.js
@@ -1,20 +1,82 @@
-'use strict';
+//'use strict';
+//
+//var wojtek = {
+//    imie: "Wojtek",
+//    wzrost: 181,
+//    przedstawOsobe: function () {
+//        console.log("Cześć jestem " + this.imie + " mam " + this.wzrost + " wzrostu.");
+//    }
+//
+//}
+//var kaja = {
+//    imie: "Kaja",
+//    wzrost: 160,
+//    przedstawOsobe: function () {
+//        console.log("Cześć jestem " + this.imie + " mam " + this.wzrost + " wzrostu.");
+//    }
+//
+//}
+//
+//console.log(wojtek.imie);
+//console.log(wojtek.wzrost);
+//
+////wojtek.imie = "Adam";
+//
+////console.log(osoba["imie"]);
+////console.log(osoba["wzrost"]);
+//
+//wojtek.przedstawOsobe();
+//
+//console.log(kaja.imie);
+//console.log(kaja.wzrost);
+//
+//kaja.przedstawOsobe();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+class Osoba {
+    constructor(imie, nazwisko, wzrost, oczy) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wzrost = wzrost;
+        this.oczy = oczy;
 
-var osoba = {
-    imie: "Wojtek",
-    wzrost: 181,
-    przedstawOsobe: function () {
-        console.log("Cześć jestem " + this.imie + " mam " + this.wzrost + " wzrostu.");
     }
-
+    wyswietlInfo() {
+        console.log("Imię: " + this.imie + "\n" + "Nazwisko: " + this.nazwisko + "\n" + "Wzrost: " + this.wzrost + "\n" + "Oczy: " + this.oczy);
+    }
 }
 
-console.log(osoba.imie);
-console.log(osoba.wzrost);
 
-osoba.imie = "Adam";
+var wojtek = new Osoba("Wojtek", "Potocki");
 
-console.log(osoba["imie"]);
-console.log(osoba["wzrost"]);
+wojtek.wzrost = 180;
+wojtek.oczy = "niebieskie";
 
-osoba.przedstawOsobe();
+
+
+
+//console.log(wojtek);
+
+var karol = new Osoba("Karol", "Młodawski");
+//console.log(karol);
+
+wojtek.wyswietlInfo();
+karol.wyswietlInfo();
