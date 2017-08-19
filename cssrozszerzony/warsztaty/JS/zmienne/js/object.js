de313234a0f7ef37adc7abd4b701718698745f02
Remigiusz Lagoda <remik22@gmail.com>
2017-08-19 12:54:14 +0200

    obj

---
+++ cssrozszerzony/warsztaty/JS/zmienne/js/object.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var osoba = {
+    imie: "Wojtek",
+    wzrost: 181,
+    przedstawOsobe: function () {
+        console.log("Cześć jestem " + this.imie + " mam " + this.wzrost + " wzrostu.");
+    }
+
+}
+
+console.log(osoba.imie);
+console.log(osoba.wzrost);
+
+osoba.imie = "Adam";
+
+console.log(osoba["imie"]);
+console.log(osoba["wzrost"]);
+
+osoba.przedstawOsobe();
