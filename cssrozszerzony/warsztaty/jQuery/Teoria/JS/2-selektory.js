12c061222126db6251c8f5a74e6b674195a56424
Remigiusz Lagoda <remik22@gmail.com>
2017-08-26 12:34:44 +0200

    selektory

---
+++ cssrozszerzony/warsztaty/jQuery/Teoria/JS/2-selektory.js
@@ -0,0 +1,17 @@
+$(function () {
+   //pobieranie elemetow po znaczniku
+
+   var paragraphs = $('p');
+   console.log(paragraphs);
+
+   //pobieranie po  id
+
+   var paragrafOne = $('#paragraf');
+   console.log(paragrafOne);
+
+   //pobieranie po klasie
+
+   var paragrafTwo = $('.paragraf');
+   console.log(paragrafTwo);
+
+});
