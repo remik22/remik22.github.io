d59113c3c486701521b858122cbe0a646ac2dc01
Remigiusz Lagoda <remik22@gmail.com>
2017-08-26 11:31:07 +0200

    kalkulator

---
+++ cssrozszerzony/warsztaty/Kalkulator odsetek/JS/main.js
@@ -0,0 +1,28 @@
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+
+   if (podatek == true) {
+      var oprocentowanieOpodatkowane = oprocentowanie * 0.81;
+   } else {
+      var oprocentowanieOpodatkowane = oprocentowanie;
+   }
+
+   var kapital = wplata * Math.pow(1 + (oprocentowanieOpodatkowane / okresKapitalizacji), iloscLat * okresKapitalizacji);
+
+   return parseFloat(kapital).toFixed(2);
+
+}
+
+
+
+function oblicz() {
+   var wplata = parseInt(document.getElementById('wplata').value);
+   var iloscLat = parseInt(document.getElementById('ilosc-lat').value);
+   var okresKapitalizacji = parseInt(document.getElementById('okres-kapitalizacji').value);
+   var oprocentowanie = parseInt(document.getElementById('oprocentowanie').value) / 100;
+   var podatek = document.getElementById('podatek').checked;
+
+   var wynik = obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
+   document.getElementById("wynik").innerHTML = wynik;
+
+
+}
--- cssrozszerzony/warsztaty/formularz/JS/script.js
+++ cssrozszerzony/warsztaty/formularz/JS/script.js
@@ -2,15 +2,36 @@
 
 
 
-var wyslijBtn = document.getElementById('wyslij');
+var wyslijBtn = document.getElementById('submit'); //przycisk submit
+var wszystkieZgody = document.getElementById('zgoda3'); //checkbox zgoda3
+
+//zaznaczenie checboxow
+function stanCheckbox() {
+   if (wszystkieZgody.checked) {
+      document.getElementById('zgoda1').checked = true;
+      document.getElementById('zgoda2').checked = true;
+
+   } else {
+      document.getElementById('zgoda1').checked = false;
+      document.getElementById('zgoda2').checked = false;
+   }
+}
+document.getElementById('zgoda3').onchange = stanCheckbox;
+
 
 function waliduj() {
    if (document.getElementById('name').value.trim() == "") {
       alert('Wypełnij imię');
    }
+   if (document.getElementById('email').value.trim() == "") {
+      alert('Wypełnij email');
+   }
+   if (document.getElementById('zgoda1').checked != true) {
+      alert('Zaznacz pierwszą zgodę')
+   }
 }
 wyslijBtn.addEventListener('click', waliduj);
-
+wszystkieZgody.addEventListener('change', stanCheckbox);
 
 
 //function checkboxState() {
